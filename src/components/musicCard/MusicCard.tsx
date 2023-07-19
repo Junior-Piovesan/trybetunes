941550586836f5433117e7265a9c118ac0527676
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-07-19 12:42:48 -0300

    Requisito 3 ok, requisito 4 em andamento criado componente header com navLink

---
+++ src/components/musicCard/MusicCard.tsx
@@ -0,0 +1,41 @@
+import { SongType } from '../../types';
+
+type PropsType = {
+  album: SongType[]
+};
+
+export default function MusicsList({ album }:PropsType) {
+  return (
+    <div>
+      {/* {album.map(({ collectionName, artworkUrl100, artistName, artistId }) => (
+        <div key={ artistId }>
+          <img
+            src={ artworkUrl100 }
+            alt={ `Capa do album da banda ${artistName}` }
+          />
+
+          <h2 data-testid="artist-name">{artistName}</h2>
+          <p data-testid="album-name">{collectionName}</p>
+        </div>
+      )).filter((music, index:number) => index === 0)} */}
+
+      <div>
+        {album.map(({ trackId, trackName, previewUrl }) => (
+          <div
+            key={ trackId }
+          >
+            <p>
+              {trackName}
+            </p>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+              .
+            </audio>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/album/Album.tsx
+++ src/pages/album/Album.tsx
@@ -2,9 +2,12 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getMusics from '../../services/musicsAPI';
 import { AlbumType, SongType } from '../../types';
+import Loading from '../../components/Loading/Loading';
+import MusicCard from '../../components/musicCard/MusicCard';
 
 export default function Album() {
-  const [musics, setMusics] = useState<SongType []>([]);
+  const [album, setAlbum] = useState<[AlbumType, ...SongType[]]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const params = useParams<{ id:string }>();
 
   useEffect(() => {
@@ -12,15 +15,34 @@ export default function Album() {
   }, []);
 
   const getListMusics = async () => {
-    const musicsList: any = await getMusics(params.id || '');
-    setMusics(musicsList);
+    const musicsList:[AlbumType, ...SongType[]] = await getMusics(params.id || '');
+    setAlbum(musicsList);
 
-    console.log(await getMusics(params.id || ''));
-
-    setMusics(musicsList);
+    setIsLoading(false);
   };
 
   return (
-    <div>Album</div>
+    <div>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <section>
+
+          {album.map(({ collectionName, artworkUrl100, artistName, artistId }) => (
+            <div key={ artistId }>
+              <img
+                src={ artworkUrl100 }
+                alt={ `Capa do album da banda ${artistName}` }
+              />
+
+              <h2 data-testid="artist-name">{artistName}</h2>
+              <p data-testid="album-name">{collectionName}</p>
+            </div>
+          )).filter((music, index:number) => index === 0)}
+
+          <MusicCard album={ album.filter((music, index) => index !== 0) } />
+          {/* <MusicCard album={ album } /> */}
+        </section>
+      )}
+    </div>
   );
 }

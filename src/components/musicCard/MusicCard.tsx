17d35dc2d86859b81afa7de0971336b8fe883596
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-07-20 12:29:06 -0300

    Requisito 5 Ok, feito logica para favoritar e desfavoritar musicas com a função isFavorite

--- src/components/musicCard/MusicCard.tsx
+++ src/components/musicCard/MusicCard.tsx
@@ -1,4 +1,5 @@
-import { MusicsType, SongType } from '../../types';
+import { useState } from 'react';
+import { SongType } from '../../types';
 import full_heart from '../../images/checked_heart.png';
 import empty_heart from '../../images/empty_heart.png';
 
@@ -6,7 +7,28 @@ type PropsType = {
   album: SongType[];
 };
 
+type EventType = {
+  target: { name:string, checked:boolean }
+};
+
 export default function MusicsList({ album }:PropsType) {
+  const [favoriteList, setIsfavorite] = useState([]);
+
+  const isFavorite = (id:number) => {
+    return favoriteList.some((favorite) => favorite === id.toString());
+  };
+
+  const hancdleChange = ({ target: { name, checked } }:EventType) => {
+    if (!isFavorite(Number(name))) {
+      const favorite = [...favoriteList, name];
+      setIsfavorite(favorite);
+    }
+    if (isFavorite(Number(name))) {
+      const favorite = favoriteList.filter((music) => music !== name);
+      setIsfavorite(favorite);
+    }
+  };
+
   return (
 
     <div>
@@ -20,8 +42,17 @@ export default function MusicsList({ album }:PropsType) {
             </p>
 
             <label data-testid={ `checkbox-music-${trackId}` } htmlFor={ trackName }>
-              <img src={ empty_heart } alt="favorite" />
-              <input type="checkbox" name={ trackName } id={ trackName } />
+              <img
+                src={ isFavorite(trackId) ? full_heart : empty_heart }
+                alt="favorite"
+              />
+              <input
+                onChange={ hancdleChange }
+                type="checkbox"
+                name={ trackId.toString() }
+                id={ trackName }
+                checked={ isFavorite(trackId) }
+              />
             </label>
 
           </div>

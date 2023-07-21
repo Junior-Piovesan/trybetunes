c1687e59931fa135dc03cf407f3c2ed29a4920ab
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-07-21 11:14:00 -0300

    Excluido linhas comentadas após refatoração

--- src/components/musicCard/MusicCard.tsx
+++ src/components/musicCard/MusicCard.tsx
@@ -1,10 +1,7 @@
-// import { useEffect, useState } from 'react';
 import { SongType } from '../../types';
 import full_heart from '../../images/checked_heart.png';
 import empty_heart from '../../images/empty_heart.png';
 import './musicCard.css';
-// import { addSong, getFavoriteSongs, removeSong } from '../../services/favoriteSongsAPI';
-// import Loading from '../Loading/Loading';
 
 type PropsType = {
   album: SongType[];
@@ -12,47 +9,7 @@ type PropsType = {
   isFavorite:(id:number) => boolean
 };
 
-// type ListFavoriteType = {
-//   trackId:number,
-//   trackName:string,
-//   previewUrl:string
-// };
-
 export default function MusicsList({ album, isFavorite, handleChange }:PropsType) {
-  // const [favoriteList, setFavoriteList] = useState<ListFavoriteType[]>([]);
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   getFavoritesList();
-  // }, []);
-
-  // const getFavoritesList = async () => {
-  //   // setIsLoading(true);
-  //   setFavoriteList(await getFavoriteSongs());
-  //   // setIsLoading(false);
-  // };
-
-  // const isFavorite = (id:number) => {
-  //   return favoriteList.some(({ trackId }) => trackId === id);
-  // };
-
-  // const hancdleChange = async (trackId:number, trackName:string, previewUrl:string) => {
-  //   if (!isFavorite(trackId)) {
-  //     const favorite:ListFavoriteType[] = [...favoriteList,
-  //       {
-  //         trackId,
-  //         trackName,
-  //         previewUrl,
-  //       }];
-  //     setFavoriteList(favorite);
-  //     await addSong({ trackId, trackName, previewUrl });
-  //   }
-  //   if (isFavorite(trackId)) {
-  //     const favorite = favoriteList.filter((music) => music.trackId !== trackId);
-  //     setFavoriteList(favorite);
-  //     await removeSong({ trackId, trackName, previewUrl });
-  //   }
-  // };
   return (
 
     <div>
@@ -74,7 +31,6 @@ export default function MusicsList({ album, isFavorite, handleChange }:PropsType
               <input
                 onChange={ () => {
                   handleChange(trackId, trackName, previewUrl);
-                  // handleClick(trackId);
                 } }
                 type="checkbox"
                 name={ trackId.toString() }
@@ -93,44 +49,6 @@ export default function MusicsList({ album, isFavorite, handleChange }:PropsType
           </audio>
         </div>
       ))}
-      {/* {isLoading ? <Loading /> : (
-        album.map(({ trackId, trackName, previewUrl }) => (
-          <div
-            key={ trackId }
-          >
-            <div>
-              <p>
-                {trackName}
-              </p>
-
-              <label data-testid={ `checkbox-music-${trackId}` } htmlFor={ trackName }>
-                <img
-                  src={ isFavorite(trackId) ? full_heart : empty_heart }
-                  alt="favorite"
-                />
-                <input
-                  onChange={ () => {
-                    hancdleChange(trackId, trackName, previewUrl);
-                    // handleClick(trackId);
-                  } }
-                  type="checkbox"
-                  name={ trackId.toString() }
-                  id={ trackName }
-                  checked={ isFavorite(trackId) }
-                  className="input__favorites"
-                />
-              </label>
-
-            </div>
-            <audio data-testid="audio-component" src={ previewUrl } controls>
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              <code>audio</code>
-              .
-            </audio>
-          </div>
-        ))
-      )} */}
     </div>
 
   );
--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import MusicCard from '../../components/musicCard/MusicCard';
 import { SongType } from '../../types';
 import { getFavoriteSongs } from '../../services/favoriteSongsAPI';
-// import full_heart from '../../images/checked_heart.png';
-// import empty_heart from '../../images/empty_heart.png';
 import Loading from '../../components/Loading/Loading';
 
 export default function Favorites() {
@@ -12,13 +10,6 @@ export default function Favorites() {
 
   useEffect(() => {
     getFavorites();
-
-    // const getFavorites = async () => {
-    //   setIsLoading(true);
-    //   const musicsList = await getFavoriteSongs();
-    //   setFavoriteList(musicsList);
-    //   setIsLoading(true);
-    // };
   }, []);
 
   const getFavorites = async () => {
@@ -46,80 +37,7 @@ export default function Favorites() {
           album={ favoriteList }
           isFavorite={ isFavorite }
         />
-      // favoriteList.map(({ trackId, trackName, previewUrl }) => (
-      //   <div
-      //     key={ trackId }
-      //   >
-      //     <div>
-      //       <p>
-      //         {trackName}
-      //       </p>
-
-      //       <label data-testid={ `checkbox-music-${trackId}` } htmlFor={ trackName }>
-      //         <img
-      //           src={ isFavorite(trackId) ? full_heart : empty_heart }
-      //           alt="favorite"
-      //         />
-      //         <input
-      //           onChange={ () => {
-      //             handleChange(trackId);
-      //           } }
-      //           type="checkbox"
-      //           name={ trackId.toString() }
-      //           id={ trackName }
-      //           checked={ isFavorite(trackId) }
-      //           className="input__favorites"
-      //         />
-      //       </label>
-
-      //     </div>
-      //     <audio data-testid="audio-component" src={ previewUrl } controls>
-      //       <track kind="captions" />
-      //       O seu navegador não suporta o elemento
-      //       <code>audio</code>
-      //       .
-      //     </audio>
-      //   </div>
-      // ))
       )}
-
-      {/* {favoriteList.map(({ trackId, trackName, previewUrl }) => (
-        <div
-          key={ trackId }
-        >
-          <div>
-            <p>
-              {trackName}
-            </p>
-
-            <label data-testid={ `checkbox-music-${trackId}` } htmlFor={ trackName }>
-              <img
-                src={ isFavorite(trackId) ? full_heart : empty_heart }
-                alt="favorite"
-              />
-              <input
-                onChange={ () => {
-                  handleChange(trackId);
-                } }
-                type="checkbox"
-                name={ trackId.toString() }
-                id={ trackName }
-                checked={ isFavorite(trackId) }
-                className="input__favorites"
-              />
-            </label>
-
-          </div>
-          <audio data-testid="audio-component" src={ previewUrl } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            <code>audio</code>
-            .
-          </audio>
-        </div>
-      ))} */}
-      {/* <MusicsList handleClick={ handleChange } album={ favoriteList } /> */}
-      {/* {isLoading ? <Loading /> : <MusicsList album={ favoriteList } />} */}
     </div>
 
   );

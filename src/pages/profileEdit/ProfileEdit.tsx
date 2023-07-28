246fa6c5e9d75aa0561f386031ea3154f8ff66d5
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-07-28 18:29:49 -0300

    estilizado componente Profile-edit, obs: falta mudar o estilo do placeholder

--- src/pages/profileEdit/ProfileEdit.tsx
+++ src/pages/profileEdit/ProfileEdit.tsx
@@ -4,6 +4,7 @@ import { UserType } from '../../types';
 import { getUser, updateUser } from '../../services/userAPI';
 import Loading from '../../components/Loading/Loading';
 import { validNameImage, validEmailDescrip } from '../../services/validations';
+import './profileEdit.css';
 
 type HandleChangeType = {
   target: { name: string, value: string },
@@ -49,60 +50,104 @@ export default function ProfileEdit() {
   const { name, email, description, image } = profile;
 
   return (
-    <section>
+    <section className="profile-edit_container">
+      <div className="header-profile-edit"> </div>
+
+      <div className="image-container">
+        <div>
+          <img className="profile-img" src={ image } alt="" />
+        </div>
+      </div>
+
       {isLoading ? <Loading /> : (
         <form
+          className="form-profile-edit"
           onSubmit={ (event) => {
             event.preventDefault();
             handleSubmit();
             navigate('/profile');
           } }
         >
-          <fieldset>
-            <legend>Editar perfil</legend>
 
+          <label
+            className="label-nome"
+            htmlFor="name"
+          >
+            Nome
+            <br />
             <input
+              className="input-nome"
               onChange={ handleChange }
               name="name"
-              placeholder="Nome"
+              id="name"
+              placeholder="Fique a vontade para usar um nome social"
               value={ profile.name }
               type="text"
               data-testid="edit-input-name"
             />
+          </label>
 
+          <label
+            className="label-email"
+            htmlFor="email"
+          >
+            Email
+            <br />
             <input
+              className="input-email"
               onChange={ handleChange }
               name="email"
-              placeholder="Email"
+              id="email"
+              placeholder="Escolha um email que consulta diariamente"
               value={ profile.email }
               type="text"
               data-testid="edit-input-email"
             />
+          </label>
 
+          <label
+            className="label-image_link"
+            htmlFor="image"
+          >
+            Imagem
+            <br />
             <input
+              className="input-image_link"
               onChange={ handleChange }
               name="image"
-              placeholder="Imagem"
+              id="image"
+              placeholder="Insira um link"
               value={ profile.image }
               type="text"
               data-testid="edit-input-image"
             />
+          </label>
+
+          <label
+            className="label-text-area"
+            htmlFor="descrição"
+          >
+            descrição
+            <br />
             <textarea
+              className="text-area"
               onChange={ handleChange }
               name="description"
-              placeholder="Descrição"
+              id="descrição"
+              placeholder="Sobre mim"
               value={ profile.description }
               data-testid="edit-input-description"
             />
+          </label>
 
-            <button
-              disabled={ !(
-                validNameImage(name, image) && validEmailDescrip(description, email)) }
-              data-testid="edit-button-save"
-            >
-              Salvar
-            </button>
-          </fieldset>
+          <button
+            className="btn-salvar"
+            disabled={ !(
+              validNameImage(name, image) && validEmailDescrip(description, email)) }
+            data-testid="edit-button-save"
+          >
+            Salvar
+          </button>
         </form>
       )}
     </section>

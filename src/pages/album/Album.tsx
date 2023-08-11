9412a27b09271a6dcdb328cae5f646c57d479c9a
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-08-11 13:15:56 -0300

    Adicionado imagens de fundo na page Album

--- src/pages/album/Album.tsx
+++ src/pages/album/Album.tsx
@@ -5,6 +5,8 @@ import { InfoAlbumType, SongType } from '../../types';
 import Loading from '../../components/Loading/Loading';
 import MusicCard from '../../components/musicCard/MusicCard';
 import { addSong, getFavoriteSongs, removeSong } from '../../services/favoriteSongsAPI';
+import gp5 from '../../images/Group 5.svg';
+import gp6 from '../../images/Group 6.svg';
 import './album.css';
 
 const initialAlbumInfo:InfoAlbumType = {
@@ -67,28 +69,39 @@ export default function Album() {
         <section>
           <div className="info-album_box">
 
+            <div className="elements-position-absolute">
+
+              <div className="big-circle-album"> </div>
+
+              <div className="small-circle-album"> </div>
+
+              <img className="gp-5-icon-album" src={ gp5 } alt="grupo 5 icone" />
+              <img className="gp-6-icon-album" src={ gp6 } alt="grupo 6 icone" />
+
+              <div className="box-artist-name-album-name">
+                <h2
+                  className="artist-name-album"
+                  data-testid="artist-name"
+                >
+                  {albumInfo.artistName}
+
+                </h2>
+                <p
+                  className="album-name-album"
+                  data-testid="album-name"
+                >
+                  { albumInfo.collectionName}
+
+                </p>
+              </div>
+
+            </div>
+
             <img
               className="album-image"
               src={ albumInfo.artworkUrl100 }
               alt={ `Capa do album da banda ${albumInfo.artistName}` }
             />
-
-            <div className="box-artist-name-album-name">
-              <h2
-                className="artist-name-album"
-                data-testid="artist-name"
-              >
-                {albumInfo.artistName}
-
-              </h2>
-              <p
-                className="album-name-album"
-                data-testid="album-name"
-              >
-                { albumInfo.collectionName}
-
-              </p>
-            </div>
 
           </div>
 

c91cb354f09f5faefeae57774c7fe52f92ae33c2
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-08-11 13:28:25 -0300

    Adicionado imagens de fundo na page Favorites

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -4,6 +4,8 @@ import { SongType } from '../../types';
 import { getFavoriteSongs, removeSong } from '../../services/favoriteSongsAPI';
 import Loading from '../../components/Loading/Loading';
 import './favorites.css';
+import gp5 from '../../images/Group 5.svg';
+import gp6 from '../../images/Group 6.svg';
 
 export default function Favorites() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -36,8 +38,18 @@ export default function Favorites() {
       {isLoading ? <Loading /> : (
         <>
           <div className="title_box">
+
+            <div className="big-circle-favorites"> </div>
+
+            <div className="small-circle-favorites"> </div>
+
+            <img className="gp-5-icon-favorites" src={ gp5 } alt="grupo 5 icone" />
+            <img className="gp-6-icon-favorites" src={ gp6 } alt="grupo 6 icone" />
+
             <h2 className="title-favorites">Músicas Favoritas</h2>
+
           </div>
+
           {favoriteList.length > 0 ? (
             <MusicCard
               handleChange={ handleChange }
